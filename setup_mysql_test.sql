b304118a58598ac56bef4ddb7881bf0813c4a967
root <jesus4388>
2022-08-08 20:21:00 -0700

    l

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- -- Create states table in hbnb_dev_db with some data
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
