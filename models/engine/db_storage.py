57712e69d4ff6638e226ec967822a53ab310f1c2
root <jesus4388>
2022-08-10 19:24:07 -0700

    j

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 '# New engine DBStorage'
 
-
 import os
 from sqlalchemy.orm import Session, scoped_session, relationship
 from sqlalchemy import create_engine
@@ -36,7 +35,7 @@ class DBStorage:
         objs = []
         dic = {}
         if cls is None:
-            str_clases = ['State', 'City']
+            str_clases = ['State', 'City', 'User', 'Amenity', 'Place', 'Review']
             for str in str_clases:
                 resul = self.__session.query(eval(str)).all()
                 for obj in resul:
