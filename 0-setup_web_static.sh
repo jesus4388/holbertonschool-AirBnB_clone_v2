f8993dd8e94061e95a1119f4677f0149380be535
jesus4388 <jesushernandez1843@gmail.com>
2022-08-25 17:53:56 +0000

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,17 +2,17 @@
 # sets up your web servers for the deployment of web_static
 sudo apt-get update -y
 sudo apt-get -y install nginx
-mkdir -p data/web_static/releases/test/
-mkdir -p data/web_static/shared/
-sudo chmod -R 777 data/web_static/releases/test/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
 printf "<html>
   <head>
   </head>
   <body>
      Holberton School
   </body>
-</html>" | sudo tee data/web_static/releases/test/index.html
-ln -sf data/web_static/releases/test data/web_static/current
-sudo chown -R ubuntu:ubuntu data/ 
+</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/ 
 sudo sed -i '/listen 80 default_server;/a locacion /hbnb_static {alias /data/web_static/current: autoindex off; }' /etc/nginx/sites-available/default 
 sudo service nginx start
