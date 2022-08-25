d9d88ab940ec0833e675f954a8b986af9ccec9c3
jesus4388 <jesushernandez1843@gmail.com>
2022-08-25 18:49:56 +0000

    l

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,6 +2,13 @@
 # sets up your web servers for the deployment of web_static
 sudo apt-get update -y
 sudo apt-get -y install nginx
+sudo chmod -R 777 /var/www/html/
+sudo echo "Hello World" | sudo tee /var/www/html/index.html
+sudo sed -i '/server_name _;/a rewrite ^/redirect_me/ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default
+sudo echo "Ceci n'est pas une page" | sudo tee /var/www/html/page.html
+sudo sed -i '/server_name _;/a error_page 404 /page.html;' /etc/nginx/sites-available/default
+var=$(cat /proc/sys/kernel/hostname)
+sudo sed -i '/http {/a add_header X-Served-By '"${var}"';' /etc/nginx/nginx.conf
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
