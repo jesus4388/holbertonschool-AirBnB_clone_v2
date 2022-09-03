bb195c695b1bb0652f07528a2ee1aa930c37deb1
jesus4388 <jesushernandez1843@gmail.com>
2022-09-03 21:44:07 +0000

    start flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 '# Starts a Flask web application'
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
-	'# definition of function'
+    '# definition of function'
     return 'Hello HBNB!'
 
 if __name__ == "__main__":
