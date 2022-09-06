e671071417435959d6457057207e8e0b5650be97
jesus4388 <jesushernandez1843@gmail.com>
2022-09-05 19:01:10 -0700

    l

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,6 +10,10 @@ import os
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+@app.teardown_appcontext
+def remove(exception=None):
+    """remove the current SQLAlchemy Session"""
+    storage.close()
 
 @app.route('/states_list', strict_slashes=False)
 def list_state():
@@ -22,10 +26,6 @@ def list_state():
         states_list.sort(key=lambda x: x.name)
         return render_template("7-states_list.html", states=states_list)
 
-@app.teardown_appcontext
-def remove(exception=None):
-    """remove the current SQLAlchemy Session"""
-    storage.close()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
