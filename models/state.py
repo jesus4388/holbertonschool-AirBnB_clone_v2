c95f324c965f5b29615029df0d73c909061c557b
jesus4388 <jesushernandez1843@gmail.com>
2022-09-05 18:23:34 -0700

    l

--- models/state.py
+++ models/state.py
@@ -14,7 +14,7 @@ class State(BaseModel, Base):
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="states", cascade="all, delete")
+        cities = relationship("City", cascade="all, delete", backref="states")
 
     if os.getenv("HBNB_TYPE_STORAGE") != 'db':
         name = ""
